00fde2d3e7f1672b6a3b7891bd66ab2a9cd2b71e
hà nguyễn <hanvph19236@fpt.edu.vn>
2022-07-16 12:58:25 +0700

    ádas

--- DuAn1_QLCHLAPTOP/Form1.Designer.cs
+++ DuAn1_QLCHLAPTOP/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(364, 205);
+            this.button1.Location = new System.Drawing.Point(152, 109);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
