8e07b9b791757cd4482e84428d9e3f728ec65839
TuanAnhPH19124 <anhntph19124@fpt.edu.vn>
2022-07-23 13:08:17 +0700

    thêm 2 class sanphammausac và mausac

---
+++ DAL/Model/MauSac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class MauSac
+    {
+        public int Id { get; set; }
+        public string MaMau { get; set; }
+        public string TenMau { get; set; }
+
+        public IList<SanPhamMauSac> sanPhamMauSacs { get; set; }
+    }
+}
---
+++ DAL/Model/SanPhamMauSac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class SanPhamMauSac
+    {
+        public int IdMau { get; set; }
+        public MauSac mauSac { get; set; }
+
+        public string MaSP { get; set; }
+        public BanPhim banPhim { get; set; }
+    }
+}
