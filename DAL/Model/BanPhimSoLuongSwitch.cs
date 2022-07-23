a43d675b7fc8a7052e952879285e55932e6dfa89
TuanAnhPH19124 <anhntph19124@fpt.edu.vn>
2022-07-23 12:57:36 +0700

    thêm class banphimsoluongswitch

---
+++ DAL/Model/BanPhimSoLuongSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class BanPhimSoLuongSwitch
+    {
+
+        public string MaSP { get; set; }
+        public BanPhim banPhim { get; set; }
+
+        public string Switch { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
