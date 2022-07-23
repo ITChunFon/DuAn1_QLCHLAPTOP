37b11ef16d395b199a53def17d3a3fe8bedc10fc
TuanAnhPH19124 <anhntph19124@fpt.edu.vn>
2022-07-23 11:11:10 +0700

    Thêm class khuyến mãi

---
+++ DAL/Model/KhuyenMai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class KhuyenMai
+    {
+        public string MaKM { get; set; }
+        public string TenKhuyenMai { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int ChietKhau { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public Boolean TrangThai { get; set; }
+        public string Mota { get; set; }
+    }
+}
